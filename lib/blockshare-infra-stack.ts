2ded75c6b8f38ab2dba06dd441fcf1e8ed945a97
Takahashi Tomohiko <takahashi@tomohiko.io>
2020-06-13 16:26:53 +0900

    init

--- lib/blockshare-infra-stack.ts
+++ lib/blockshare-infra-stack.ts
@@ -1,9 +1,26 @@
-import * as cdk from '@aws-cdk/core';
+// https://qiita.com/yacchin/items/e2c48984a3a68e77c3c5
+// https://dev.classmethod.jp/articles/ecs-deploy-using-cdk/
+// Ref: https://github.com/aws-samples/aws-cdk-examples/blob/master/typescript/ecs/fargate-application-load-balanced-service/index.ts
+import ec2 = require('@aws-cdk/aws-ec2');
+import ecs = require('@aws-cdk/aws-ecs');
+import ecs_patterns = require('@aws-cdk/aws-ecs-patterns');
+import cdk = require('@aws-cdk/core');
 
 export class BlockshareInfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create VPC and Fargate Cluster
+    // NOTE: Limit AZs to avoid reaching resource quotas
+    const vpc = new ec2.Vpc(this, 'MyVpc', { maxAzs: 2 });
+    const cluster = new ecs.Cluster(this, 'Cluster', { vpc });
+
+    // Instantiate Fargate Service with just cluster and image
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "FargateService", {
+      cluster,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+      },
+    });
   }
 }
