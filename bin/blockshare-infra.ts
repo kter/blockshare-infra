812079d5b26a414c6e8610f0c065672148f151db
Takahashi Tomohiko <takahashi@tomohiko.io>
2020-06-14 16:13:58 +0900

    region指定

--- bin/blockshare-infra.ts
+++ bin/blockshare-infra.ts
@@ -4,4 +4,9 @@ import * as cdk from '@aws-cdk/core';
 import { BlockshareInfraStack } from '../lib/blockshare-infra-stack';
 
 const app = new cdk.App();
-new BlockshareInfraStack(app, 'BlockshareInfraStack');
+new BlockshareInfraStack(app, 'BlockshareInfraStack', {
+    env: {
+     region: 'ap-northeast-1',
+    }
+});
+
