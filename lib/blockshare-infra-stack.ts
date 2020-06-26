f3d5d687fee647f6f3f48523ed4463252a45384f
Takahashi Tomohiko <takahashi@tomohiko.io>
2020-06-27 00:51:44 +0900

    無理でした

--- lib/blockshare-infra-stack.ts
+++ lib/blockshare-infra-stack.ts
@@ -7,6 +7,9 @@ import ecr = require('@aws-cdk/aws-ecr');
 import ecs_patterns = require('@aws-cdk/aws-ecs-patterns');
 import cdk = require('@aws-cdk/core');
 import iam = require('@aws-cdk/aws-iam');
+import secretsManager = require('@aws-cdk/aws-secretsmanager');
+import rds = require('@aws-cdk/aws-rds');
+import ssm = require('@aws-cdk/aws-ssm');
 
 export class BlockshareInfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -19,6 +22,39 @@ export class BlockshareInfraStack extends cdk.Stack {
       vpc: vpc,
       clusterName: 'blockshare'
     });
+
+    const mySqlPassword = new secretsManager.Secret(this, "MySqlPassword", {
+      secretName: `blockshare-web-credentials`,
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({
+          username: 'blockshare-admin',
+        }),
+        excludePunctuation: true,
+        includeSpace: false,
+        generateStringKey: 'password',
+      }
+    });
+    new ssm.StringParameter(this, 'DBCredentialsArn', {
+      parameterName: `blockshare-web-credentials-arn`,
+      stringValue: mySqlPassword.secretArn,
+    });
+    const mySql = new rds.DatabaseCluster(this, "rds", {
+      clusterIdentifier: `blockshare-web-cluster`,
+      engine: rds.DatabaseClusterEngine.AURORA,
+      engineVersion: '5.7.mysql_aurora.2.08.1',
+      masterUser: {
+        username: 'admin',
+      },
+      masterUserPassword: mySqlPassword.secretValueFromJson('password'),
+      scalingConfiguration: {
+        autoPause: true,
+        maxCapacity: 4,
+        minCapacity: 2,
+        secondsUntilAutoPause: 3600,
+      },
+      deletionProtection: false
+    });
+
     const execution_role = new iam.Role(this, 'executionRole', {
       roleName: 'blockshare-task-role',
       assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
@@ -41,6 +77,16 @@ export class BlockshareInfraStack extends cdk.Stack {
       image: ecs.ContainerImage.fromEcrRepository(repository, 'latest'),
       logging: new ecs.AwsLogDriver({ streamPrefix: 'blockshare-web' }),
       command: ['rails', 's', '-p', '80', '-b', '0.0.0.0'],
+      secrets: {
+        // MYSQL_PASSWORD: ecs.Secret.FromSecretsManager(mySqlPassword),
+        MYSQL_PASSWORD: mySqlPassword.secretValueFromJson('password'),
+      },
+      environment: {
+        RAILS_ENV: "development",
+        MYSQL_DATABASE: 'blockshare',
+        MYSQL_HOST: mySql.DbInstanceEndpointAddresIs,
+        MYSQL_USER: mySqlPassword.secretValueFromJson('username').toString(),
+      },
     }).addPortMappings({
       containerPort: 80,
       hostPort: 80,
