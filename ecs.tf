869c9a5aa8e84822020bee7b9f411e9976c51d69
Takahashi Tomohiko <takahashi@tomohiko.io>
2020-06-28 16:30:48 +0900

    不要記述を削除

--- ecs.tf
+++ ecs.tf
@@ -85,12 +85,3 @@ module "ecs_task_execution_role" {
   policy     = data.aws_iam_policy_document.ecs_task_execution.json
 }
 
-resource "aws_ecs_task_definition" "example" {
-  family                   = "example"
-  cpu                      = "256"
-  memory                   = "512"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  container_definitions    = file("./container_definitions.json")
-  execution_role_arn       = module.ecs_task_execution_role.iam_role_arn
-}
