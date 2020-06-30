77fb838c52935be6bc2a9f71da10533418da03ca
Takahashi Tomohiko <takahashi@tomohiko.io>
2020-07-01 08:00:14 +0900

    ELBに接続できない問題を解決

--- network.tf
+++ network.tf
@@ -30,6 +30,12 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 }
 
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  gateway_id         = aws_internet_gateway.main.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
 resource "aws_route_table_association" "public_0" {
   subnet_id      = aws_subnet.public_0.id
   route_table_id = aws_route_table.public.id
